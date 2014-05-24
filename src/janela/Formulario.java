647e0039e2a43946240df9f20bc15de8f95ec5d6
edveloso <edveloso@gmail.com>
2014-05-24 07:48:03 -0300

    merging conflit resolving

--- src/janela/Formulario.java
+++ src/janela/Formulario.java
@@ -1,10 +1,15 @@
 package janela;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Formulario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
+		
 
 	}
+	
 
 }
